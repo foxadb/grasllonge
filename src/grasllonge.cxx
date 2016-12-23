8e62604bb3e5415f0fbc63b61e833129be321a4f
Malhorne <malhorne.desroches@gmail.com>
2016-12-23 19:29:04 +0100

    Basic parser

--- src/grasllonge.cxx
+++ src/grasllonge.cxx
@@ -14,4 +14,60 @@
  * =====================================================================================
  */
 
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <cstdlib>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+int main(int argc, char* argv[])
+{
+	//Flush the history
+	rl_clear_history();	
+	const char *prompt = "grasllonge>";
+	const std::string	add("add"),
+						print("print");
+
+	while(1)
+	{
+		char* line;
+		line = readline(prompt);
+
+		if (line==0 || !strncmp(line, "exit", 4))
+		{
+			std::cout << "\nBye :)" << std::endl;
+			break;
+		}
+
+		add_history(line);
+
+		std::istringstream iss(line);
+		std::vector<std::string> tokens;
+
+		do
+		{
+			std::string sub;
+			iss >> sub;
+			tokens.push_back(sub);
+		} while (iss);
+
+		std::cout << "You idiot wrote: " << std::endl;
+		for(size_t i=0; i<tokens.size(); ++i)
+		{
+			std::cout << tokens[i] << ' ';
+		}
+		std::cout << std::endl;
+
+		if (tokens[0] == add)
+			std::cout << "You chose the add option" << std::endl;
+		else if (tokens[0] == print)
+			std::cout << "You chose the print option" << std::endl;
+		else 
+			std::cout << "This option is unknown to grasllonge" << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
