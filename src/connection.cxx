06d08017d1dc6c0a869c76af6039a1bd71db46ce
Malhorne <malhorne.desroches@gmail.com>
2016-12-23 21:50:08 +0100

    Ajout

---
+++ src/connection.cxx
@@ -0,0 +1,15 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  connection.cxx
+ *
+ *    Description:  Fichier contenant les fichiers d'interaction avec Challonge
+ *
+ *        Version:  1.0
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+
+#include "connection.h"
---
+++ src/connection.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  connection.h
+ *
+ *    Description:  Header pour le fichier qui va contenir les fonctions qui vont interragir
+ *					avec Challonge
+ *
+ *        Version:  1.0
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+#ifndef CONNECTION_H
+#define CONNECTION_H 
+
+#endif /* CONNECTION_H */
